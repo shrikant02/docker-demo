0ef1bcf271590acfac494b00531600e0467d0834
shrikant <shrikantnavghare2001@gmail.com>
2021-07-23 18:31:41 +0530

    first

---
+++ src/test/java/com/shrikant/dockerdemo/DockerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shrikant.dockerdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
